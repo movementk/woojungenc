86e8ae03f7ea9cf6a6aa9acf3a4b496761c973f8
kimyh5392 <mk.kimyh@gmail.com>
2017-10-12 15:18:27 +0900

    product > view 퍼블리싱 완료

--- product/view.php
+++ product/view.php
@@ -18,11 +18,38 @@
             <article class="product-promotion">
                 <div class="promotion-slide">
                     <div class="container">
-                        <ul>
-                            <li>
-                                
-                            </li>
-                        </ul>
+                        <p class="figure">
+                            <img src="/assets/images/product/product_img01.jpg" class="img-responsive" alt="">
+                        </p>
+                        <div class="pager-poromotion">
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        <img src="/assets/images/product/product_img01.jpg" class="img-responsive" alt="">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="/assets/images/product/product_img01.jpg" class="img-responsive" alt="">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="/assets/images/product/product_img01.jpg" class="img-responsive" alt="">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="/assets/images/product/product_img01.jpg" class="img-responsive" alt="">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="/assets/images/product/product_img01.jpg" class="img-responsive" alt="">
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
                 <div class="product-purchase">
@@ -30,22 +57,160 @@
                         <h3><img src="/assets/images/product/logo.png" alt=""></h3>
                         <h4 class="product-title">Green Line GM500</h4>
                         <p class="summary">
-                            Sealsafe Plus는 특허받은 케이지 실링 디자인을<br>
-                            통한사용자 및 실험동물의 완벽한 보호를 제공합니다.
+                            Sealsafe Plus는 특허받은 케이지 실링 디자인을<br class="visible-xs">
+                            통한사용자 및 실험동물의<br class="hidden-xs"> 완벽한 보호를 제공합니다.
                         </p>
                         <div class="btn-area">
                             <p>
                                 <a href="#" class="btn btn-lg btn-default" role="button">견적문의</a>
                                 <a href="#" class="btn btn-lg btn-default" role="button">바로구매</a>
-                                <a href="#" class="btn btn-lg btn-white" role="button"><i class="icon-"></i></a>
+                                <a href="#" class="btn btn-lg btn-white" role="button">
+                                    <i class="icon-cart">
+                                        <span class="sr-only">장바구니 담기</span>
+                                    </i>
+                                </a>
                             </p>
                         </div>
                     </div>
                 </div>
             </article>
+            <div class="product-infor">
+                <div class="container">
+                    <article class="row">
+                        <div class="col-xs-12 col-sm-6">
+                            <div class="infor-1">
+                                <p class="figure">
+                                    <img src="/assets/images/product/infor_img01.jpg" class="img-responsive" alt="">
+                                </p>
+                                <div class="details">
+                                    <h5 class="title">01. 랫치가 필요없는 인체공학적인 케이지</h5>
+                                    <p>
+                                        최상의 밀폐를 보장하는 특허받은 GM500으로<br>
+                                        작업자와 실험동물 모두의 안전이 보장됩니다.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6">
+                            <div class="infor-2">
+                                <p class="figure">
+                                    <img src="/assets/images/product/infor_img02.jpg" class="img-responsive" alt="">
+                                </p>
+                                <div class="details">
+                                    <h5 class="title">02. 동물의 스트레스를 최소화시키는 공기 흐름</h5>
+                                    <p>
+                                        케이지 상단에 위치한 급/배기 밸브는 공기의 난류를<br>
+                                        일으키지 않아 열 손실과 동물 스트레스에 대한<br class="hidden-lg">
+                                        위험을 최소화 시킵니다.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                    <article class="infor-3">
+                        <p class="figure">
+                            <img src="/assets/images/product/infor_xs_img03.jpg" class="img-responsive visible-xs" alt="">
+                            <img src="/assets/images/product/infor_sm_img03.jpg" class="img-responsive visible-sm visible-md" alt="">
+                            <img src="/assets/images/product/infor_lg_img03.jpg" class="img-responsive visible-lg" alt="">
+                        </p>
+                        <div class="details">
+                            <h5 class="title">03. 향상된 케이지 밀도</h5>
+                            <p>
+                                새로운 케이지 하부구조와<br class="visible-xs">
+                                낮아진 커버로 2미터의<br class="visible-sm visible-md"> 랙에<br class="visible-xs">
+                                10열의 케이지를 수납할<br class="visible-xs">
+                                수 있습니다.
+                            </p>
+                        </div>
+                    </article>
+                    <article class="infor-4">
+                        <div class="details">
+                            <h5 class="title">04. 25% 시간 절감된 케이지 교체 작업</h5>
+                            <p>
+                                랙에 비주얼 인디케이터가 있어 신속하게 케이지 도킹<br class="hidden-lg">
+                                상태를 확인할 수 있습니다.<br class="visible-lg"> 실리콘 가스켓이 있는<br class="hidden-lg">
+                                외삽형 물병은 셀프 센터링을 적용하여 작업자<br class="hidden-lg">
+                                능률을 향상시킵니다.
+                            </p>
+                        </div>
+                        <p class="figure">
+                            <img src="/assets/images/product/infor_img04.jpg" class="img-responsive" alt="">
+                        </p>
+                    </article>
+                </div>
+            </div>
         </div>
 	</main>
 	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/footer.php"); ?>
 	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/docfoot.php"); ?>
+	<script src="/assets/jquery.bxslider/jquery.bxslider.min.js"></script>
+	<script>
+		(function($) {
+            var promotionSlider = {
+                slideWidth: 60,
+                minSlides: 2,
+                maxSlides: 3,
+                moveSlides: 1,
+                slideMargin: 20,
+                pager: false,
+                prevText: '<span><i class="icon-left-open-1"></i></span>',
+                nextText: '<span><i class="icon-right-open-1"></i></span>'
+            }
+            
+            var prmSlider = $('.promotion-slide .pager-poromotion ul').bxSlider(promotionSlider);
+            
+            $(window).on('load resize', function() {
+                var conWidth = $('.container:eq(0)').outerWidth();
+                prmSlider.reloadSlider(promotionSlider);
+                if ( conWidth >= 750 ) {
+                    prmSlider.reloadSlider({
+                        pager: false,
+                        slideWidth: 80,
+                        minSlides: 5,
+                        maxSlides: 5,
+                        moveSlides: 1,
+                        slideMargin: 25,
+                        prevText: '<span><i class="icon-left-open-1"></i></span>',
+                        nextText: '<span><i class="icon-right-open-1"></i></span>'
+                    });
+                }
+                if ( conWidth >= 1200 ) {
+                    prmSlider.reloadSlider({
+                        pager: false,
+                        slideWidth: 80,
+                        minSlides: 4,
+                        maxSlides: 4,
+                        moveSlides: 1,
+                        slideMargin: 25,
+                        prevText: '<span><i class="icon-left-open-1"></i></span>',
+                        nextText: '<span><i class="icon-right-open-1"></i></span>'
+                    });
+                }
+            });
+            
+//                if ( conWidth >= 970 ) {
+//                    prmSlider.reloadSlider({
+//                        pager: false,
+//                        slideWidth: 200,
+//                        minSlides: 4,
+//                        maxSlides: 4,
+//                        moveSlides: 1,
+//                        slideMargin: 0
+//                    });
+//                }
+            /*
+            $('.promotion-slide .pager-poromotion ul').bxSlider({
+                slideWidth: 60,
+                minSlides: 2,
+                maxSlides: 3,
+                moveSlides: 1,
+                slideMargin: 20,
+                pager: false,
+                prevText: '<span><i class="icon-left-open-1"></i></span>',
+                nextText: '<span><i class="icon-right-open-1"></i></span>'
+            });
+            */
+        })(jQuery);
+	</script>
 </body>
 </html>
